1ee796decc831a998c77a186db4b06e12bd16152
Ed Beroset <beroset@ieee.org>
2022-05-11 12:05:04 -0400

    Use Gray code search pattern for prefix table
    This uses a much more efficient search for the xor combination of
    remainder values which speeds up the code by about an order of
    magnitude.

--- src/crc.cpp
+++ src/crc.cpp
@@ -1,5 +1,7 @@
 #include "crc.h"
 #include <numeric>
+#include <algorithm>
+#include <vector>
 
 #include <iostream>
 #include <iomanip>
@@ -94,16 +96,70 @@ constexpr uint16_t test_prefix(uint16_t bits, const std::array<uint16_t, 16> pre
  */
 uint16_t find_prefix(std::size_t len, uint16_t target) {
     std::array<uint16_t, 16> prefixtable{};
+    std::array<uint16_t, 16> bitselect{};
     auto v = prefixtable[0] = prefix(1, len);
+    auto s = bitselect[0] = 0x0001;
     for (unsigned i{1}; i < 16; ++i) {
         prefixtable[i] = (v << 1) ^ ((v & 0x8000u) ? 0x1021 : 0);
         v = prefixtable[i];
+        s = bitselect[i] = (s << 1);
     }
-    for (uint16_t prefix{0xffff}; prefix; --prefix) {
-        if (target == test_prefix(prefix, prefixtable)) {
-            return prefix;
+    /* 
+     * The goal here is to come up with the combination of 
+     * prefixtable entries that xor together to the target value.
+     *
+     * To minimize the time for these we use a Gray code.
+     * Recall that a Gray code is an ordering such that successive
+     * terms differ by exactly one bit.
+     *
+     * We use this so that we only require one XOR operation 
+     * of the current testvalue with one of the entries in the
+     * prefixtable array.
+     *
+     * Each iteration, we generate the entry number that should
+     * change, which corresponds with a bit position in the prefix
+     * and also as an index into the prefixtable array.
+     *
+     * The recursive definition for generating this sequence for
+     * any m-bit Gray code is this:
+     *
+     *   void gray(int m) {
+     *       if (m > 0) {
+     *           gray(m-1);
+     *           std::cout << (m-1) << '\n';
+     *           gray(m-1);
+     *       }
+     *   }
+     *
+     * That's compact but not very efficient, so we use a vector
+     * as a stack to do the same thing in an iterative rather than
+     * recursive manner.
+     */
+    uint16_t prefix{0};
+    uint16_t testvalue{0};
+    std::vector<int> work(17);
+    std::generate(work.begin(), work.end(), [n=17]() mutable { return --n; });
+    while (!work.empty()) {
+        int m = work.back();
+        work.pop_back();
+        if (m == 0) {
+            m = work.back() - 1;
+            work.pop_back();
+            // we have exhausted all possibilities
+            if (m > 0x8000u) {
+                return 0;
+            }
+            testvalue ^= prefixtable[m];
+            prefix ^= bitselect[m];
+            // we've found our target
+            if (testvalue == target) {
+                return prefix;
+            }
+            for (auto i{m+1}; i; --i) {
+                work.push_back(i-1);
+            }
         }
     }
-    return 0;
+    return 0;   // should never get here
 }
 }
